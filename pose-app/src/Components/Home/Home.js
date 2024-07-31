f697afd5395f4e236bf8a28ad1d9c1d4aa6e9a5e
CodeRay Test <coderaytest@gmail.com>
2024-07-31 12:23:19 +0530

    try with tailwind1

--- pose-app/src/Components/Home/Home.js
+++ pose-app/src/Components/Home/Home.js
@@ -3,8 +3,105 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+      <div className='flex justify-between items-center mb-4'>
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new The new supermac The new supermac is The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 here, 543 cv and 140 000$. This is best racing computer about 7 is here, 543 cv and 140 000$. This is best racing computer about 7 supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+      </div>
+      <div className='flex justify-between items-center'>
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" class="block w-full h-full">
+                <img alt="blog photo" src="/assets/images/tom1.jpg" class="object-cover w-full max-h-40"/>
+                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                    <p class="font-medium text-indigo-500 text-md">
+                    </p>
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                        New Mac is here !
+                    </p>
+                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
+                        The new supermac is here, 543 cv and 140 000$. This is best racing computer about 7 years on...
+                    </p>
+                </div>
+            </a>
+        </div>
+      </div>
+
     </div>
   );
 };
